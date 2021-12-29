e533d84002db9338f473f504c4b2a6a728a09c26
ZedPro <contact@zedpro.me>
2021-12-29 22:03:29 +0800

    Fix typo is variable name from coloumn_b to colomn_a

--- xlsx.py
+++ xlsx.py
@@ -4,13 +4,13 @@ import openpyxl
 def read_settlement(xlsx_file_location):
     settlement_file = openpyxl.load_workbook(xlsx_file_location)
     sheet = settlement_file.active
-    column_b = list()
+    column_a = list()
     column_l = list()
     for cell in sheet['A']:
-        column_b.append(cell.value)
+        column_a.append(cell.value)
     for cell in sheet['L']:
         column_l.append(cell.value)
-    settlement_list = list(zip(column_b[1:], column_l[1:]))
+    settlement_list = list(zip(column_a[1:], column_l[1:]))
     return settlement_list
 
 
