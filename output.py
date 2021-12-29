afdbb435cd5830d9dcba5ebb37060b1e00640587
ZedPro <contact@zedpro.me>
2021-12-29 19:51:46 +0800

    Add checks for amount of campaigns in report Change text from bill to campaign to ensure similar nomenclature across labels.

--- output.py
+++ output.py
@@ -3,7 +3,13 @@ computed_sum = compute_settlement_sum(settlement_data)
 
 
 def bill_count():
-    print(f"There are {len(computed_sum)} bills in the settlement file provided.\n")
+    if len(computed_sum) == 1:
+        print(f"There is {len(computed_sum)} campaign in the settlement file provided.\n")
+    elif len(computed_sum) < 1:
+        print(f"There are no campaigns in the settlement file provided.\n")
+    else:
+        print(f"There are {len(computed_sum)} campaigns in the settlement file provided.\n")
+
 
 
 def bill_total():
